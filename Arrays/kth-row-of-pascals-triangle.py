215b0817de0ce0463ff67b91fbc33c6b861cdbab
Vivek Moar <44374277+vivekiitm@users.noreply.github.com>
2020-02-06 16:33:29 +0530

    Create kth-row-of-pascals-triangle.py

---
+++ Arrays/kth-row-of-pascals-triangle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getRow(self, A):
+        A+=1
+        a=[]
+        for i in range(A):
+            a.append([0]*(i+1))
+        for i in range(A):
+            for j in range(i+1):
+                if(j==0 or j==i):
+                    a[i][j]=1
+                else:
+                    a[i][j]=a[i-1][j-1]+a[i-1][j]
+        return a[A-1]
