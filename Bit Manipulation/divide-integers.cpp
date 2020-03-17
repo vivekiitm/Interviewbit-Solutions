4f510f9271cc892f1794c127ebe8cd3e02f9f713
Vivek Moar <44374277+vivekiitm@users.noreply.github.com>
2020-03-17 23:09:03 +0530

    Update divide-integers.cpp

--- Bit Manipulation/divide-integers.cpp
+++ Bit Manipulation/divide-integers.cpp
@@ -1,10 +1,51 @@
+typedef long long ll;
+int Solution::divide(int A, int B) {
+if(A == INT_MIN && B == -1)
+return INT_MAX;
+int sign;
+ll p = A;
+ll q = B;
+if((p < 0 && q < 0) || (p > 0) && (q > 0))
+{
+if(p < 0 && q < 0)
+{
+p = (-1)*p;
+q = (-1)*q;
+}
+sign = 0;
+}
+else
+{
+if(p < 0)
+p = (-1)*p;
+else if(q < 0)
+q = (-1)*q;
+sign = 1;
 
-int Solution::divide(int B, int A) {
-    int ans=0;
-    while(B<A)
+}
+
+ll a = p;
+ll b = q;
+ll ans = 0;
+while(a >= q)
+{
+    b = q;
+    ll cou = 0;
+    while(b <= a)
     {
-        B=B-A;
-        ans++;
+        cou++;
+        b <<= 1;
     }
+    ans |= (1 << (cou-1));
+    a = a - (b>>=1);
+}
+if(ans >= INT_MAX)
+    return INT_MAX;
+else
+{
+    if(sign == 1)
+    return (-1 * ans);
+    else
     return ans;
+}
 }
