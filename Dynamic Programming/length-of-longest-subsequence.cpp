994b5f4caec5ba0f449541856f6cd145c2db5e47
Vivek Moar <44374277+vivekiitm@users.noreply.github.com>
2020-04-25 14:11:30 +0530

    Update length-of-longest-subsequence.cpp

--- Dynamic Programming/length-of-longest-subsequence.cpp
+++ Dynamic Programming/length-of-longest-subsequence.cpp
@@ -1,38 +1,38 @@
-/*
-Given an array of integers, find the length of longest subsequence which is first increasing then decreasing.
-
-**Example: **
-
-For the given array [1 11 2 10 4 5 2 1]
-
-Longest subsequence is [1 2 10 4 2 1]
-
-Return value 6
-
-*/
-
 int Solution::longestSubsequenceLength(const vector<int> &A) {
     int n=A.size();
-    int lis[n];
-    int lds[n];
-    for(int i=0;i<n;i++){
-     lis[i]=1;
-     lds[i]=1;
+    if(n==0)
+        return 0;
+    if(n==1)
+        return 1;
+    int dpi[n];
+    int dpd[n];
+    for(int i=0;i<n;i++)
+    {
+        dpi[i]=1;dpd[i]=1;
     }
-    for(int i=1;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(A[i]>A[j]&&lis[i]<lis[j]+1)
-             lis[i]=lis[j]+1;
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(A[i]>A[j])
+                if(dpi[j]>=dpi[i])
+                    dpi[i]=dpi[j]+1;
         }
     }
-    for(int i=n-2;i>=0;i--){
-        for(int j=n-1;j>i;j--){
-            if(A[i]>A[j]&&lds[i]<lds[j]+1)
-             lds[i]=lds[j]+1;
+    for(int i=n-1;i>=0;i--)
+    {
+        for(int j=n-1;j>i;j--)
+        {
+            if(A[i]>A[j])
+                if(dpd[j]>=dpd[i])
+                    dpd[i]=dpd[j]+1;
         }
     }
     int ans=0;
     for(int i=0;i<n;i++)
-     ans=max(ans,lis[i]+lds[i]-1);
+    {
+        if(ans<dpi[i]+dpd[i]-1)
+            ans=dpi[i]+dpd[i]-1;
+    }
     return ans;
 }
